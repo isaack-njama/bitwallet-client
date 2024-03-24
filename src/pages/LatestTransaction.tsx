c1145b52259c7383f2fd1d6cdab75668289c05bb
Isaack Njama <isaacknjama@proton.me>
2024-03-24 17:04:57 +0300

    feat: improve latest transaction page

--- src/pages/LatestTransaction.tsx
+++ src/pages/LatestTransaction.tsx
@@ -23,8 +23,14 @@ const LatestTransaction = () => {
     <IonContent fullscreen>
       <>
         <div style={{ display: 'flex', gap: '60px', margin: '60px' }}>
-          <img src='/Button.svg' style={{ margin: 'auto' }} />
-          <img src='/Button (1).svg' style={{ margin: 'auto' }} />
+          <img
+            src='/Button.svg'
+            style={{ margin: 'auto', cursor: 'pointer' }}
+          />
+          <img
+            src='/Button (1).svg'
+            style={{ margin: 'auto', cursor: 'pointer' }}
+          />
         </div>
         <PageHeader
           pageTitle='Your balance'
@@ -43,55 +49,59 @@ const LatestTransaction = () => {
             }}
           >
             <img src='/Circle arrow up.svg' />
-            Review your security settings to best protect your funds.
+            <IonLabel style={{ fontSize: '18px' }}>
+              Review your security settings to best protect your funds.
+            </IonLabel>
             <img
               src='/chevron-forward-outline.svg'
               style={{ width: '30px', height: '30px' }}
             />
           </IonCardContent>
         </IonCard>
-        <IonList style={{ background: 'none' }}>
-          <IonListHeader>
-            <IonLabel style={{ margin: '60px', fontWeight: 'bold' }}>
-              Latest transactions
-            </IonLabel>
-          </IonListHeader>
-          <IonItem
-            style={{ maxWidth: '540px', maxHeight: '76px', margin: 'auto' }}
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            margin: '24px',
+          }}
+        >
+          <IonLabel style={{ fontWeight: 'bold' }}>
+            Latest transactions
+          </IonLabel>
+        </div>
+        <IonCard>
+          <IonCardContent
+            style={{
+              display: 'flex',
+              gap: '24px',
+              justifyContent: 'space-between',
+            }}
           >
-            <IonLabel
+            <img src='/Item left accessory.svg' />
+            <div
               style={{
                 display: 'flex',
-                gap: '54px',
-                justifyContent: 'center',
+                flexDirection: 'column',
+                maxWidth: '335px',
+                maxHeight: '76px',
               }}
             >
-              <img src='/Item left accessory.svg' />
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  maxWidth: '335px',
-                  maxHeight: '76px',
-                }}
-              >
-                <h4>bc1q gx9g ... sfsy</h4>
-                <h6>2 min ago</h6>
-              </div>
-              <h4 style={{ color: '#27AE60' }}>+50,000 sats</h4>
-            </IonLabel>
-          </IonItem>
-        </IonList>
+              <h4 style={{ fontSize: '18px' }}>bc1q gx9g ... sfsy</h4>
+              <h6>2 min ago</h6>
+            </div>
+            <h4 style={{ color: '#27AE60' }}>+50,000 sats</h4>
+          </IonCardContent>
+        </IonCard>
       </>
       <div
         style={{
           position: 'fixed',
           bottom: 0,
-          width: '100%',
+          left: '240px',
+          maxWidth: '100%',
           display: 'flex',
           justifyContent: 'center',
-          gap: '360px',
-          margin: '16px',
+          marginBottom: '16px',
           cursor: 'pointer',
         }}
       >
