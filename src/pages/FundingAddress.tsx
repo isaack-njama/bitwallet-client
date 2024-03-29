0cec8bdcfefd7c82ff32f67252a7a016e45b27c1
Isaack Njama <isaacknjama@proton.me>
2024-03-29 06:39:17 +0300

    refactor(FundingAddress): fetch from endpoint and display as QR code

--- src/pages/FundingAddress.tsx
+++ src/pages/FundingAddress.tsx
@@ -2,27 +2,22 @@ import { IonContent, IonPage } from '@ionic/react';
 
 import Button from '../components/Button';
 import FundingAddress from '../components/FundingAddress';
+import { useEffect, useState } from 'react';
 
 const FundingAddressPage: React.FC = () => {
+  const [address, setAddress] = useState<string>('');
+
   return (
     <IonPage>
       <IonContent fullscreen slot='fixed'>
         <FundingAddress
           title='Deposit to this Bitcoin address'
           subtitle='Fees may apply.'
-          address='bc1qv8prsqt80e9775llmujwat9yx78tgn6q2x0l3t'
+          address={address || 'No Address to display'}
         />
       </IonContent>
       <div style={{ display: 'flex' }}>
-        <Button
-          text='Share'
-          icon={
-            <img
-              src='/public/share-outline.svg'
-              style={{ background: 'red' }}
-            />
-          }
-        />
+        <Button text='Share' icon={<img src='/public/share-outline.svg' />} />
         <Button text='Copy' />
       </div>
     </IonPage>
